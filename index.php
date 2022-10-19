20ad9460a449b85acb4629c94b3596c5c1b5bbca
ika1155 <ishidas1155@gmail.com>
2022-10-19 14:21:22 +0900

    update index.php

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
 $month = 0;
 
+if (isset($_GET['month']) && is_numeric($_GET['month']))
+{
+	$month = (int)$_GET['month'];
+}
+
 $date = new DateTime('first day of this month');
 $date->setTimezone(new DateTimeZone('Asia/Tokyo'));
 
-$firstDay = $date->format('w');
+if ($month < 0)
+{
+	$date->sub(new DateInterval("P" . (0 - $month) . "M"));
+}
+else if (0 < $month )
+{
+	$date->add(new DateInterval('P'.$month."M"));
+}
+
+//週初めが何曜日か数字で表示
+$firstDayOfWeek = $date->format('w'); 
+//月末が何日か（ひと月に何日あるか）
 $endOfTheMonth = $date->format('t');
+
+$weeks = ceil(($firstDayOfWeek + $endOfTheMonth) / 7);
+$day = 1;
+
 //https://wepicks.net/phpsample-date-format/#page-003
 //https://dobon.net/vb/dotnet/system/newdatetime.html 指定した日付のDatetime
 ?>
@@ -17,6 +37,14 @@ $endOfTheMonth = $date->format('t');
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>07-5 Calender</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+	<style>
+        th {
+            text-align: center;
+        }
+        td {
+            text-align: right;
+        }
+    </style>
 </head>
 <body>
 	<div class="container">
@@ -38,19 +66,17 @@ $endOfTheMonth = $date->format('t');
 							<th>金</th>
 							<th>土</th>	
 						</tr>
-						
-						<?php $day = 1;
-						?>
-						
-						<?php for ($i=0; $i<6; $i++): ?>
+
+						<?php for ($i = 0; $i < $weeks; $i++): //週　?>
 							<tr>
-							<?php for ($j=0; $j<7; $j++): ?>
-								
+							<?php for ($j = 0; $j < 7; $j++): //日　?>								
 								<?php 
-								if($i == 0 && $firstDay == $j){
+								//1週目かつ曜日が一日と同じとき
+								if($i == 0 && $firstDayOfWeek == $j){
 									echo '<td>'.$day.'</td>';
 									$day++;
 								}
+								//2日目以降かつ月末未満の時
 								else if(1 < $day && $day <= $endOfTheMonth){
 									echo '<td>'.$day.'</td>';
 									$day++;
@@ -60,7 +86,6 @@ $endOfTheMonth = $date->format('t');
 									echo '<td></td>';
 								}
 								?>
-								
 							<?php endfor ?>
 							</tr>
 						<?php endfor ?>
@@ -68,9 +93,9 @@ $endOfTheMonth = $date->format('t');
 					</table>
 				</div>
 				<div class="card-footer">
-					<a href="./" class="btn btn-outline-primary">&lt;&lt;前の月</a>
+					<a href="./?month= <?= $month - 1 ?>" class="btn btn-outline-primary">&lt;&lt;前の月</a>
 					<a href="./" class="btn btn-primary">今月</a>
-					<a href="./" class="btn btn-outline-primary">次の月&gt;&gt;</a>
+					<a href="./?month= <?= $month + 1 ?>" class="btn btn-outline-primary">次の月&gt;&gt;</a>
 				</div>
 			</div>
 		</div>
